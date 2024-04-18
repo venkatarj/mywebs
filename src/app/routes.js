30edcb0a235f93a66a6325d2e42592956ba3a2fa
Venkata Rohith Jujjuru <venkatarohithjujjuru@Venkatas-MacBook-Air.local>
2024-04-17 17:32:34 -0700

    first commit

---
+++ src/app/routes.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Route, Routes} from "react-router-dom";
+import withRouter from "../hooks/withRouter"
+import { Home } from "../pages/home";
+import { Socialicons } from "../components/socialicons";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
+const AnimatedRoutes = withRouter(({ location }) => (
+  <TransitionGroup>
+    <CSSTransition
+      key={location.key}
+      timeout={{
+        enter: 400,
+        exit: 400,
+      }}
+      classNames="page"
+      unmountOnExit
+    >
+      <Routes location={location}>
+        <Route exact path="/" element={<Home />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </CSSTransition>
+  </TransitionGroup>
+));
+
+function AppRoutes() {
+  return (
+    <div className="s_c">
+      <AnimatedRoutes />
+      <Socialicons />
+    </div>
+  );
+}
+
+export default AppRoutes;
---
+++ src/content_option.js
@@ -0,0 +1,184 @@
+const logotext = "JV";
+const meta = {
+    title: "Rohith JV",
+    description: "I’m Rohith ,currently purusing my masters in computer engineering at ASU.",
+};
+
+const introdata = {
+    title: "I’m Rohith",
+    animated: {
+        first: "I love coding",
+        second: "Code, circuits, and creativity.",
+        third: "Bringing innovation to life with embedded systems.",
+    },
+    description: `Embedded Systems Engineer | Currently pursuing MS in Computer Engineering (Electrical Engineering focus) at ASU | Formerly employed at
+TCS`,
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [{
+        jobtitle: "Developer",
+        where: "Tata Consultancy Services Pvt Ltd.",
+        date: "Aug,2021 - Aug,2022",
+        description: 
+        <><p>Conducted Data validation, reconciliation achieving a 99.5% in data integrity accuracy for a trillion-dollar cap client.</p>
+    <p>Optimized Docker configuration, resulting in a 30% reduction in deployment time and improved system stability by 15%.</p>
+    <p>Integrated JIRA and Git, reducing project delivery time by 20% through enhanced collaboration and code management.</p></>
+          ,
+    },
+    {
+        jobtitle:'Graduate Student Assistant',
+        where:'Arizona State University',
+        date:'Aug,2023 - May,2024',
+        description:'Worked on the monitoring & assessing of multiple embedded system design projects for UG students in Senior Design Lab.',
+    }
+    
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "MongoDB",
+        value: 85,
+    },
+    {
+        name: "AWS",
+        value: 80,
+    },
+    {
+        name: "ETL Tools",
+        value: 60,
+    },
+    {
+        name: "MATLAB",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Custom Cache Replacement Policy Implementation: ",
+        description: <><p>Developed and deployed a bespoke cache replacement policy in Gem5, leveraging ARM Cortex processors and Real-Time 
+Operating Systems for optimized cache management.</p>
+<p>• Exhibited C++ expertise by integrating FIFO, LRU, and a modified cache policy optimized for ARM Cortex architecture.</p>
+<p>• Implemented State Machine logic for dynamic cache management, ensuring adaptability to system states and workloads.</p>
+<p>• Employed advanced debugging techniques, including real-time monitoring and analysis, to identify and address performance 
+bottlenecks, resulting in enhanced cache efficiency and overall system performance in Gem5.</p>
+Technologies: Python, C++, ARM, GEM5, RTOS.</>
+    
+    },
+    {
+        title: "Smart Building Energy Management System:",
+        description: <><p>Developed a system using ARM Cortex-M series microcontrollers and integrated actuators for HVAC and lighting control.</p>
+<p>• Utilized FreeRTOS for real-time task scheduling, ensuring system stability, and integrated Wi-Fi for connectivity with building 
+management systems and cloud platforms.</p>
+<p>• Integrated energy monitoring circuitry to measure electricity consumption, facilitating data-driven energy optimization.</p>
+<p>• Developed a user-friendly application for monitoring, scheduling, and notifications, with support for firmware OTA updates.
+Technologies: Arduino, FreeRTOS, C++, Tkinter.</p> </>
+    },
+    {
+        title: "Virtual gesture Recognition Using OpenCV Machine Learning Software:",
+        description: <><p>Developed a vision-based gesture and object recognition system to empower computer control through gestures and reduced 
+the time spent on data input by 25% for users with physical disabilities, improving overall experience and productivity.</p>
+<p>• Engineered gesture recognition model with 94.5% accuracy and 30 ms frame processing for real-time responsiveness.</p>
+<p>• Curated and processed a 10,000-sample gesture dataset, achieving 97% accuracy post-model fine-tuning.</p>
+Technologies: Python, TensorFlow, OpenCV, Deep Learning, CNN.
+
+        </>},
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "venkatarohithj@gmail.com",
+    YOUR_FONE: "(480)-238-9775",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/venkatarj",
+    linkedin: "https://www.linkedin.com/in/venkata-rohith-jujjuru/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
